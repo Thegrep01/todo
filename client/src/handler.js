7de18259d534ba20a3472c729d985c35348189cb
thegrep <den.belotserkovets@gmail.com>
2018-11-07 17:41:27 +0200

    add front-edn ver 0.1

---
+++ client/src/handler.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+
+const handler = new Vue()
+
+export default handler
--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './components/App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: 'app',
+  created: function () {
+    console.log('root created')
+  },
+  comments: {
+    App
+  },
+  methods: {}
 })
