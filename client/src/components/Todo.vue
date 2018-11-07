3a07e50c6ee5a6aec7e57caab60f1cd9dc3539f1
thegrep <den.belotserkovets@gmail.com>
2018-11-07 23:18:12 +0200

    add getAll()

--- client/src/components/Todo.vue
+++ client/src/components/Todo.vue
@@ -7,6 +7,18 @@
             <button class="btn primary-btn">Add</button>
         </form>
        </div>
+       <div class="container">
+         <ul>
+           <li v-for="todo in todos" :key="todo.id">
+             <p>
+               <label>
+                 <input type="checkbox" :checked=todo.status @change="todo.status = !todo.status"/>
+                 <span>{{todo.title}}</span>
+               </label>
+             </p>
+           </li>
+         </ul>
+       </div>
     </div> 
 </template>
 
@@ -20,20 +32,34 @@ export default {
       Todo: {
         title: "",
         status: false
-      }
+      },
+      todos: []
     };
+  },
+  created: function() {
+    this.getAll();
   },
   methods: {
     submitToDo() {
-      let url = "https://server-hfdjjaiktt.now.sh/home/add";
-      axios
-        .post(url, this.Todo)
-        .then(res => {
-          console.log(res);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      if (this.Todo.title != "") {
+        let url = "https://server-hfdjjaiktt.now.sh/home/add";
+        axios
+          .post(url, this.Todo)
+          .then(res => {
+            console.log(res);
+            this.Todo.title = "";
+            this.getAll();
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+    },
+    getAll() {
+      let url = "https://server-hfdjjaiktt.now.sh/home/all";
+      axios.get(url).then(res => {
+        this.todos = res.data;
+      });
     }
   }
 };
